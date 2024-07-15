d83a7724b8a2512b7a75a5cc0c958526333436d4
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-07-15 22:02:00 +0200

    Add error logic

--- src/components/LogIn/LogIn.tsx
+++ src/components/LogIn/LogIn.tsx
@@ -1,101 +1,3 @@
-// import { useForm } from "react-hook-form";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { authenticationSchema, FormValues } from "../../utils/zod";
-// import { useState } from "react";
-// import logo from "../../assets/logo.png";
-// import styles from "./LogIn.module.css";
-// import Input from "../Input/Input";
-// import Button from "../Button/Button";
-// import { initialUserState } from "../../utils/constants";
-// import { UserType } from "../../utils/types";
-
-// const LogIn = () => {
-//   const [user, setUser] = useState<UserType>(initialUserState);
-
-//   const {
-//     register,
-//     handleSubmit,
-//     watch,
-//     formState: { errors },
-//   } = useForm<FormValues>({
-//     defaultValues: {
-//       email: user.email,
-//       password: user.password,
-//     },
-//     resolver: zodResolver(authenticationSchema),
-//   });
-
-//   const watchEmail = watch('email')
-//   const watchPassword = watch("password")
-
-//   console.log(watchEmail)
-
-//   const onSubmit = async (data: FormValues) => {
-//     console.log(data)
-//     try {
-//       if (
-//         user.email === "mariomilosevic888@gmail.com" &&
-//         user.password === "123456789"
-//       )
-//         console.log("tacno");
-//     } catch (error) {
-//       console.log("netacno");
-//       console.log(errors)
-//     }
-//   };
-
-//   return (
-//     <div className={styles.container}>
-//       <img src={logo} alt={logo} className={styles.logo} />
-//       <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-//         <Input
-//           placeholder={"Enter your email here"}
-//           title={"Email"}
-//           type={"text"}
-//           value={watchEmail}
-//           changeHandler={(e) =>
-//             setUser((prev) => ({
-//               ...prev,
-//               email: e.target.value,
-//             }))
-//           }
-//           zod={{
-//             ...register("email", {
-//               required: {
-//                 value: true,
-//                 // message:""
-//               },
-//             }),
-//           }}
-//         />
-//         <Input
-//           placeholder={"Enter your password here"}
-//           title={"Password"}
-//           type={"password"}
-//           value={watchPassword}
-//           changeHandler={(e) =>
-//             setUser((prev) => ({
-//               ...prev,
-//               password: e.target.value,
-//             }))
-//           }
-//           zod={{
-//             ...register("password", {
-//               required: {
-//                 value: true,
-//               },
-//             }),
-//           }}
-//         />
-
-//         <Button text="Confirm" />
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default LogIn;
-
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { authenticationSchema, FormValues } from "../../utils/zod";
@@ -109,11 +11,11 @@ import { UserType } from "../../utils/types";
 
 const LogIn = () => {
   const [user, setUser] = useState<UserType>(initialUserState);
+  const [loginError, setLoginError] = useState<string>("");
 
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm<FormValues>({
     defaultValues: {
@@ -123,19 +25,20 @@ const LogIn = () => {
     resolver: zodResolver(authenticationSchema),
   });
 
-  const watchEmail = watch("email");
-  const watchPassword = watch("password");
-
   const onSubmit = async (data: FormValues) => {
     console.log(data);
     try {
-      if (
-        data.email === "mariomilosevic888@gmail.com" &&
-        data.password === "123456789"
-      ) {
-        console.log("Login successful");
+      if (data.email === "mariomilosevic887@gmail.com") {
+        console.log("tacan email");
       } else {
-        console.log("Invalid credentials");
+        console.log("Email nije dobar");
+        setLoginError("Email nije dobar");
+      }
+      if (data.password === "123456789") {
+        console.log("tacan pasvord");
+      } else {
+        console.log("Pasvord nije dobar");
+        setLoginError("Pasvord nije dobar");
       }
     } catch (error) {
       console.log("An error occurred during login");
@@ -151,42 +54,31 @@ const LogIn = () => {
           placeholder="Enter your email here"
           title="Email"
           type="text"
-          value={watchEmail}
+          value={user.email}
           changeHandler={(e) =>
             setUser((prev) => ({
               ...prev,
               email: e.target.value,
             }))
           }
-          zod={{
-            ...register("email", {
-              required: "Email is required",
-            }),
-          }}
+          zod={{ ...register("email") }}
         />
-        {errors.email && (
-          <span className={styles.error}>{errors.email.message}</span>
-        )}
+        {errors.email && <span>{errors.email.message}</span>}
         <Input
           placeholder="Enter your password here"
           title="Password"
           type="password"
-          value={watchPassword}
+          value={user.password}
           changeHandler={(e) =>
             setUser((prev) => ({
               ...prev,
               password: e.target.value,
             }))
           }
-          zod={{
-            ...register("password", {
-              required: "Password is required",
-            }),
-          }}
+          zod={{ ...register("password") }}
         />
-        {errors.password && (
-          <span className={styles.error}>{errors.password.message}</span>
-        )}
+        {errors.password && <span>{errors.password.message}</span>}
+        {loginError && <div className={styles.error}>{loginError}</div>}
 
         <Button text="Confirm" />
       </form>
--- src/utils/zod.ts
+++ src/utils/zod.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const authenticationSchema = z.object({
-  email: z.string().email({ message: "Account not found !" }),
-  password: z.string().min(6, { message: "Incorrect password !" }),
+  email: z.string().email({ message: "Please enter a valid email address." }),
+  password: z.string().min(6, { message: "Password must be at least 6 charachters." }),
 });
 
 export type FormValues = z.infer<typeof authenticationSchema>
