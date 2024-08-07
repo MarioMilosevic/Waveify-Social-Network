518f56758c88bc8d9a78519eaccae9181e22598f
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-08-07 15:49:13 +0200

    Add delete button

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -2,15 +2,22 @@ import styles from "./Comment.module.css";
 import UserHeader from "../UserHeader/UserHeader";
 import { formatDate } from "../../utils/helperFunction";
 import { SingleCommentType } from "../../utils/types";
+import { FaTrash } from "react-icons/fa";
 
 const Comment = ({ comment }: SingleCommentType) => {
-  console.log(comment)
+  console.log(comment);
   const { created_at, text } = comment;
   const formattedDate = formatDate(created_at);
   return (
     <div className={styles.container}>
       <UserHeader user={comment} formattedDate={formattedDate} />
-      <p>{text}</p>
+      <div className={styles.text_container}>
+        <p>{text}</p>
+        <div className={styles.button_container}>
+          <FaTrash />
+          <button>Delete</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/UserHeader/UserHeader.tsx
+++ src/components/UserHeader/UserHeader.tsx
@@ -2,16 +2,12 @@ import styles from "./UserHeader.module.css";
 import { LiaCalendarWeekSolid } from "react-icons/lia";
 import { dateIconSize } from "../../utils/constants";
 import { UserHeaderProps } from "../../utils/types";
-const UserHeader = ({ user, formattedDate }:UserHeaderProps) => {
-  const {picture, full_name, username} = user
+const UserHeader = ({ user, formattedDate }: UserHeaderProps) => {
+  const { picture, full_name, username } = user;
   return (
     <div className={styles.header}>
       <div className={styles.user_container}>
-        <img
-          src={picture}
-          alt={picture}
-          className={styles.profile_image}
-        />
+        <img src={picture} alt={picture} className={styles.profile_image} />
         <div className={styles.user_info}>
           <h3>{`@${username}`}</h3>
           <h2>{full_name}</h2>
