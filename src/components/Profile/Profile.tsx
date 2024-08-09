2c63ba730cbd250cabbfc2a91ffb5dc80a9d5ee0
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-08-09 21:47:38 +0200

    Add log out and profile icons

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,8 +1,10 @@
 import styles from "./Profile.module.css";
 import marioImage from "../../assets/mariomilosevic.jpg";
+import { CgProfile } from "react-icons/cg";
+import { IoMdExit } from "react-icons/io";
 import { useState } from "react";
 const Profile = () => {
-  const [isVisible, setIsVisible] = useState<boolean>(false)
+  const [isVisible, setIsVisible] = useState<boolean>(false);
   return (
     <div className={styles.container}>
       <div className={styles.image_container}>
@@ -10,12 +12,19 @@ const Profile = () => {
           src={marioImage}
           alt={marioImage}
           className={styles.profile_image}
-          onClick={() => setIsVisible(prev => !prev)}
+          onClick={() => setIsVisible((prev) => !prev)}
         />
-      <div className={isVisible ? styles.visible : styles.not_visible}>
-        <div>My profile</div>
-        <div>Log out</div>
-      </div>
+        <div className={isVisible ? styles.visible : styles.not_visible}>
+          <div className={styles.profile_div}>
+
+            <CgProfile className={styles.profile_icon} />
+            <span>My profile</span>
+          </div>
+          <div className={styles.log_out}>
+            <IoMdExit className={styles.log_out_icon} />
+            <span>Log out</span>
+          </div>
+        </div>
       </div>
     </div>
   );
