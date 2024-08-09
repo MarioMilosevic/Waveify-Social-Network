1303e434e86d59000e307ae1d40637f9c309857f
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-08-09 21:26:02 +0200

    Add profile animation

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,10 +1,21 @@
 import styles from "./Profile.module.css";
 import marioImage from "../../assets/mariomilosevic.jpg";
+import { useState } from "react";
 const Profile = () => {
+  const [isVisible, setIsVisible] = useState<boolean>(false)
   return (
     <div className={styles.container}>
       <div className={styles.image_container}>
-        <img src={marioImage} alt={marioImage} />
+        <img
+          src={marioImage}
+          alt={marioImage}
+          className={styles.profile_image}
+          onClick={() => setIsVisible(prev => !prev)}
+        />
+      <div className={isVisible ? styles.visible : styles.not_visible}>
+        <div>My profile</div>
+        <div>Log out</div>
+      </div>
       </div>
     </div>
   );
