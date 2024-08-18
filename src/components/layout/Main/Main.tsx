853f98e3efdb6d393da18b2093a610787c2ed75f
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-08-18 18:29:54 +0200

    Add footer

--- src/components/layout/Main/Main.tsx
+++ src/components/layout/Main/Main.tsx
@@ -4,6 +4,7 @@ import NewPost from "../NewPost/NewPost";
 import Posts from "../Posts/Posts";
 import MainContainer from "../MainContainer/MainContainer";
 import { usePostsSlice } from "../../../hooks/usePostsSlice";
+import Footer from "../Footer/Footer";
 const Main = () => {
   const { posts } = usePostsSlice();
   return (
@@ -14,6 +15,7 @@ const Main = () => {
           <Post key={post.post_id} post={post} />
         ))}
       </Posts>
+      <Footer/>
       <ToastContainer style={{ fontSize: "1.6rem" }} />
     </MainContainer>
   );
